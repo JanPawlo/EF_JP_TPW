deff12b1bee21aa8671b8027eb54514e7c557513
Emil Franczak <251511@edu.p.lodz.pl>
2025-04-13 18:59:57 +0200

    Layered structure project

---
+++ ProjectTPW/ModelData/Ball.cs
@@ -0,0 +1,13 @@
+﻿namespace Data
+{
+    public class Ball
+    {
+        public int id { get; set; }
+        public double x { get; set; }
+        public double y { get; set; }
+        public double Radius { get; set; } = 10;
+        public string Color { get; set; } = "Red";
+
+
+    }
+}
