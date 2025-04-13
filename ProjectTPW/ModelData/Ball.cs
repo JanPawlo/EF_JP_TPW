2d3576895cfa9717adc7c61f525ae86e2f30fe54
Emil Franczak <251511@edu.p.lodz.pl>
2025-04-13 19:24:46 +0200

    Progress on Data Layer

--- ProjectTPW/ModelData/Ball.cs
+++ ProjectTPW/ModelData/Ball.cs
@@ -5,8 +5,7 @@
         public int id { get; set; }
         public double x { get; set; }
         public double y { get; set; }
-        public double Radius { get; set; } = 10;
-        public string Color { get; set; } = "Red";
+        public double Radius { get; set; }
 
 
     }
