b45825d49312d818ca927e0b27ed64b191a51435
Jan <251608@edu.p.lodz.pl>
2025-04-14 00:01:46 +0200

    wyswietlenie ramki, przycisk z rekacja

--- ProjectTPW/PresentationView/MainWindow.xaml.cs
+++ ProjectTPW/PresentationView/MainWindow.xaml.cs
@@ -1,24 +1,23 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using System;
 
 namespace PresentationView
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
         }
+
+
+        // Reakcja na wcisniecie przycisku
+        private void btn0_Click(object sender, RoutedEventArgs e)
+        {
+            // Odwolanie do obiektow z xaml'a za pomoca atrybutu "Name"
+            testHello.Text = "New Hello!";
+        }
+
     }
 }
