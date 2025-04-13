39d273c0a22486355c19b154d25f67c744d8cada
Emil Franczak <251511@edu.p.lodz.pl>
2025-04-13 19:42:49 +0200

    Small fixes

---
+++ ProjectTPW/Data/DataImplementation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    internal class DataImplementation : DataAbstractAPI
+    {
+        public override void Start(int numOfBalls)
+        {
+            // Implementacja metody Start
+            // TBD
+            Console.WriteLine($"Starting with {numOfBalls} balls.");
+        }
+    }
+}
